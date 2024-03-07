e61241861565696c55a2a78aaf03662b9085b1ef
Dylan Ennis <dennis@ucsd.edu>
2024-03-07 00:44:29 -0800

    fixed some formatting in code

--- controllers/places.js
+++ controllers/places.js
@@ -1,46 +1,36 @@
 const router = require("express").Router();
 const express = require("express");
-const places = require('../models/places.js')
+const places = require("../models/places.js");
 
-router.get('/', (req, res) => {
-    res.render('places/index', { places })
-})
-
-// More code ...
+router.get("/", (req, res) => {
+  res.render("places/index", { places });
+});
 
 
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
 // or switching new.jsx to views foler and removing the \places or \places/ works as well (try both)
-
-router.get("/new", (req, res) => {
-  res.render("places/new");
-});
+ 
 
 // create
 
-
-
-router.post('/', (req, res) => {
-  console.log(req.body)
+router.post("/", (req, res) => {
+  // console.log(req.body)
   if (!req.body.pic) {
     // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
+    req.body.pic = "http://placekitten.com/400/400";
   }
   if (!req.body.city) {
-    req.body.city = 'Anytown'
+    req.body.city = "Anytown";
   }
   if (!req.body.state) {
-    req.body.state = 'USA'
+    req.body.state = "USA";
   }
-  places.push(req.body)
-  res.redirect('/places')
-})
-
-
+  places.push(req.body);
+  res.redirect("/places");
+});
 
 router.use("/places", router);
 
 module.exports = router;
-
--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.static('public'))
 
 // Controllers & Routes
 
-
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
